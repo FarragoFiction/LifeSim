832a9482c59fd86c9ea64aed66053b34c00033be
Jenny <gtg287y@gmail.com>
2018-04-21 12:42:29 -0400

    betting hooked up

--- web/scripts/Controllers/blackJack.dart
+++ web/scripts/Controllers/blackJack.dart
@@ -2,19 +2,71 @@ import 'dart:html';
 import "../LifeSimLib.dart";
 import 'dart:async';
 import "package:BlackJack/BlackJack.dart";
+import "dart:math" as Math;
 
 Game game;
+Element div;
+int bet = 113;
 void main() {
     loadNavbar();
-    start();
+    div = querySelector("#output");
+    drawBetButton();
+}
+
+void clearDiv() {
+    List<Element> children = new List.from(div.children);
+    children.forEach((f) {
+        f.remove();
+    });
+}
+
+void drawBetButton() {
+    clearDiv();
+    ButtonElement betButton = new ButtonElement();
+    betButton.text = "Bet";
+    InputElement value = new InputElement();
+    value.value = "$bet";
+    value.min = "$bet";
+    int max = Math.max(bet, CardLibrary.money);
+    value.max = "${max}";
+
+    SpanElement valueMarker = new SpanElement();
+    valueMarker.text = "$bet";
+
+    value.type = "range";
+    div.append(valueMarker);
+    div.append(value);
+    div.append(betButton);
+
+    value.onChange.listen((e) {
+        valueMarker.text = value.value;
+        bet = int.parse(value.value);
+    });
+
+    betButton.onClick.listen((e) {
+        start();
+    });
 }
 
 Future<Null> start() async{
+    clearDiv();
+    CardLibrary.money = CardLibrary.money + -1* bet;
     await Loader.preloadManifest();
-    game = new Game(Card.getFreshDeck(),querySelector("#output"), finishGame);
+    game = new Game(Card.getFreshDeck(),div, finishGame);
     game.start();
 }
 
 void finishGame() {
-    window.alert("TODO: process bets");
+    String result = "You lost, thems the breaks.";
+    if(!game.lost) {
+        int winnings = 2* bet;
+        result = ("You won! ${winnings} Life Bux");
+        CardLibrary.money = CardLibrary.money + winnings;
+    }
+    ButtonElement restartButton = new ButtonElement();
+    restartButton.text = "New Deal?";
+    div.append(restartButton);
+    restartButton.onClick.listen((e) {
+        drawBetButton();
+    });
 }
