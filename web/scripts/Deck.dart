10d5b3ee00027998f4a063362c522b23e5120438
jadedResearcher <gtg287y@gmail.com>
2018-08-31 11:16:29 -0400

    simple fucking animation, now lunch

--- web/scripts/Deck.dart
+++ web/scripts/Deck.dart
@@ -12,9 +12,10 @@ class Deck {
     static String ALTERNIA = "ALTERNIA";
     static int boosterSize = 13;
     static Map<String, Deck> _allDecks;
-    static List<String> allDeckNames = <String>["misc", "alternia"];
+    static List<String> allDeckNames = <String>["misc", "alternia","sburb"];
     static List<String> alternianSubDecks = <String>["burgundy","bronze","gold","lime","olive","jade","teal","cerulean","indigo","purple","violet","fucshia","mutant"];
     String name;
+    AudioElement soundEffects = new AudioElement();
 
     List<String> _cards;
 
@@ -49,22 +50,34 @@ class Deck {
         return _allDecks;
     }
 
-    Future<Element> makeShopElement() async{
+    //240776__f4ngy__card-flip
+    void playSoundEffect(String locationWithoutExtension) {
+        if(soundEffects.canPlayType("audio/mpeg").isNotEmpty) soundEffects.src = "music/${locationWithoutExtension}.mp3";
+        if(soundEffects.canPlayType("audio/ogg").isNotEmpty) soundEffects.src = "music/${locationWithoutExtension}.ogg";
+        soundEffects.play();
+    }
+
+    Future<Element> makeShopElement(Element decks, Element purchasedCards) async{
         DivElement container = new DivElement()..classes.add("deck");
+        decks.append(container);
         ImageElement tmp = await image;
         container.append(image);
         List<String> tmp2 = await cards;
         DivElement stats = new DivElement()..text = "$name: ${tmp2.length} cards";
         container.append(stats);
-        makeButtons(container);
+        makeButtons(container, purchasedCards);
         return container;
     }
 
-    void makeButtons(Element container) {
+    void makeButtons(Element container, Element purchasedCards) {
         ButtonElement boosterButton = new ButtonElement()..text = "Buy Booster For $boosterCost";
         ButtonElement deckButton = new ButtonElement()..text = "Buy Deck For $deckCost";
         container.append(boosterButton);
         container.append(deckButton);
+
+        boosterButton.onClick.listen((Event e){
+            processBooster(purchasedCards);
+        });
     }
 
 
@@ -91,6 +104,25 @@ class Deck {
             }
         }
         return _cards;
+    }
+
+    Future<Null> processBooster(Element container) async {
+        DivElement element = new DivElement();
+        container.append(element);
+        List<GenericScene> chosen = await makeBooster();
+        for(GenericScene scene in chosen) {
+            print("drawing card for $scene");
+            await new Future.delayed(const Duration(seconds: 1), () => animateBoosterCard(scene,container));
+            //new Timer(new Duration(milliseconds: 1000), () => animateBoosterCard(scene,container));
+        }
+    }
+
+    Future<Null> animateBoosterCard(GenericScene scene, Element container) async{
+        //240776__f4ngy__card-flip
+        playSoundEffect("240776__f4ngy__card-flip");
+        await scene.drawCard(container, -13);
+        print("done animating booster card for $scene");
+
     }
 
     Future<List<GenericScene>> makeBooster() async {
--- web/scripts/store.dart
+++ web/scripts/store.dart
@@ -1,15 +1,24 @@
 import 'Deck.dart';
+import 'Scenes/SceneFactory.dart';
+import 'Stats/StatFactory.dart';
 import 'dart:async';
 import 'dart:html';
+import 'navbar.dart';
 
 
 
 DivElement output = querySelector("#output");
 Future<Null> main() async{
+    loadNavbar();
+    StatFactory.initAllStats();
+    SceneFactory.initScenes();
+    Element purchasedCards = new DivElement()..classes.add("purchased")..text = "You got:";
+    Element availableDecks = new DivElement();
+    output.append(purchasedCards);
+    output.append(availableDecks);
     Map<String, Deck> decks = Deck.allDecks();
     for(Deck d in decks.values) {
         print("loaded deck ${d.name}");
-        Element e = await d.makeShopElement();
-        output.append(e);
+        await d.makeShopElement(availableDecks, purchasedCards);
     }
 }
